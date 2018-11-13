3a469206629c24c83c212660f6f590fd0558e56f
Casey <Casey@DESKTOP-61H6QIQ.fios-router.home>
2018-11-13 15:51:29 -0500

    created a scanner

--- src/WhichSeason.java
+++ src/WhichSeason.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+/**
+ * 
+ * @author Casey Capetola
+ *
+ */
+public class WhichSeason 
+{
 
-public class WhichSeason {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner user = new Scanner(System.in);
+		
 
 	}
 
