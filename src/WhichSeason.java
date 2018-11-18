7077709ab9a12a8fc177826f5ba982db115be294
Casey <Casey@DESKTOP-61H6QIQ.fios-router.home>
2018-11-18 13:14:17 -0500

    Completed Assignment

--- src/WhichSeason.java
+++ src/WhichSeason.java
@@ -10,7 +10,30 @@ public class WhichSeason
 	public static void main(String[] args) 
 	{
 		Scanner user = new Scanner(System.in);
+		System.out.print("Enter the numerical value of your birthmonth: ");
+		int birthMonth = Integer.parseInt(user.nextLine());
 		
+		if(birthMonth>=3 && birthMonth<=5)
+		{
+			System.out.print("\nYou were born in spring.");
+		}
+		
+		else if(birthMonth>=6 && birthMonth<=8)
+		{
+			System.out.print("\nYou were born in summer.");
+		}
+		else if(birthMonth>=9 && birthMonth<=11)
+		{
+			System.out.print("\nYou were born in autumn.");
+		}
+		else if(birthMonth==12 || birthMonth<=2 && birthMonth>=1)
+		{
+			System.out.print("\nYou were born in winter.");
+		}
+		else
+		{
+			System.out.print("\nThe number you entered is invalid.");
+		}
 
 	}
 
